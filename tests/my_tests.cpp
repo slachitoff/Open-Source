af956cb5785db895fc7d82f4ce5b9cd2cfb3bad4
slachitoff <sky.achitoff@gmail.com>
2024-01-27 18:09:10 -0500

    Integrated Google Test

---
+++ tests/my_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+// Your test cases go here
+TEST(MyTestSuite, Test1) {
+    // Your test assertions go here
+    EXPECT_EQ(10 * 10, 100);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
