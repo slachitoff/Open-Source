a55636fab3d8882786f166fbb62aac0a94495cd8
slachitoff <sky.achitoff@gmail.com>
2024-01-28 19:41:21 -0500

    Remove build directory from repository

--- build/MyTests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/Sky/Documents/GitHub/Open-Source/build/MyTests[1]_tests.cmake")
-  include("C:/Users/Sky/Documents/GitHub/Open-Source/build/MyTests[1]_tests.cmake")
-else()
-  add_test(MyTests_NOT_BUILT MyTests_NOT_BUILT)
-endif()
--- build/MyTests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[MyTestSuite.Test1]=]  C:/Users/Sky/Documents/GitHub/Open-Source/build/Debug/MyTests.exe [==[--gtest_filter=MyTestSuite.Test1]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[MyTestSuite.Test1]=]  PROPERTIES WORKING_DIRECTORY C:/Users/Sky/Documents/GitHub/Open-Source/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  MyTests_TESTS MyTestSuite.Test1)
