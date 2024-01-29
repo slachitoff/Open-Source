d60b61e518a4f8254ce90a471c45760efeb0d5d7
slachitoff <sky.achitoff@gmail.com>
2024-01-28 20:10:07 -0500

    Update my_tests.cpp

--- tests/my_tests.cpp
+++ tests/my_tests.cpp
@@ -6,7 +6,7 @@ TEST(MyTestSuite, Test1) {
     EXPECT_EQ(10 * 10, 100);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
