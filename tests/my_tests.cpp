62dddf5fd10f5f4a3b460f6c966afcaa1ebbfae1
slachitoff <sky.achitoff@gmail.com>
2024-02-01 14:35:31 -0500

    Apply clang-format

--- tests/my_tests.cpp
+++ tests/my_tests.cpp
@@ -2,11 +2,11 @@
 
 // Your test cases go here
 TEST(MyTestSuite, Test1) {
-    // Your test assertions go here
-    EXPECT_EQ(10 * 10, 100);
+  // Your test assertions go here
+  EXPECT_EQ(10 * 10, 100);
 }
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
